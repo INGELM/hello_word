185ba0920fe1ae986a3f51cbee841ca4688bb6ca
ingelm <henrymoraleshl@gmail.com>
2025-05-31 15:26:55 -0400

    init

---
+++ passenger_wsgi.py
@@ -0,0 +1,5 @@
+from run import app as application
+# This file is used by the WSGI server to run the Flask application.
+
+# The application object is named 'application' to be compatible with WSGI standards.
+# The 'application' variable is the entry point for the WSGI server to run the Flask app.
